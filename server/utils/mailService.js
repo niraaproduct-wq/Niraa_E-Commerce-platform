14d3447c9ee85794a4f4c5dcd03d329e76f07582
VASANTHKUMAR R <vasanthkumar.2205155@srec.ac.in>
2026-05-04 19:24:46 +0530

    Fix: Switch to SMTP port 587 and add connection timeouts

--- server/utils/mailService.js
+++ server/utils/mailService.js
@@ -5,13 +5,15 @@ const sendEmailOTP = async (email, otp) => {
     // Note: These env variables should be added to your .env file
     const transporter = nodemailer.createTransport({
       host: 'smtp.gmail.com',
-      port: 465,
-      secure: true,
-      family: 4, // Force IPv4 to avoid ENETUNREACH on environments like Render
+      port: 587,
+      secure: false, // true for 465, false for other ports
+      family: 4, // Force IPv4
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
       },
+      connectionTimeout: 10000, // 10 seconds
+      greetingTimeout: 10000,
     });
 
     const mailOptions = {
